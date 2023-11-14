891a92759dc7788f9502f53b3e8a6256ebdd5535
Paolo Innocenti <paonath@gmail.com>
2023-11-14 10:38:37 +0100

    refactored structure

---
+++ BuildCurrentChangelog/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PH.ChangeLogs;
+
+var md = PH.ChangeLogs.CurrentChangelog.Changelog.ToMarkdown();
+await System.IO.File.WriteAllTextAsync(@"../../../../PH.ChangeLogs/changelog.md", md);
